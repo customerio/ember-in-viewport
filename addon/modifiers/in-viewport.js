2fa62fc1fd4d34612c8d3299fc2c3299dbebfc72
Adrian Zalewski <bardzusny@users.noreply.github.com>
2022-10-18 21:58:01 +0200

    Support Ember 4.x: updating ember-modifiers, removing deprecated APIs & switching to the new "modify" hook.

--- addon/modifiers/in-viewport.js
+++ addon/modifiers/in-viewport.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 import { DEBUG } from '@glimmer/env';
 import Modifier from 'ember-modifier';
 import deepEqual from 'fast-deep-equal';
+import { registerDestructor } from '@ember/destroyable';
 
 const WATCHED_ELEMENTS = DEBUG ? new WeakSet() : undefined;
 
@@ -11,57 +12,71 @@ export default class InViewportModifier extends Modifier {
   @service inViewport;
 
   name = 'in-viewport';
+  isInstalled = false;
 
   lastOptions;
+  options;
 
-  get options() {
+  onEnterArg;
+  onExitArg;
+
+  constructor(owner, args) {
+    super(owner, args);
+    registerDestructor(this, this.destroyWatcher);
+  }
+
+  setOptions(named) {
     // eslint-disable-next-line no-unused-vars
-    const { onEnter, onExit, ...options } = this.args.named;
-    return options;
+    const { onEnter, onExit, ...options } = named;
+
+    this.lastOptions = this.options;
+
+    this.options = options;
+    this.onEnterArg = onEnter;
+    this.onExitArg = onExit;
   }
 
   get hasStaleOptions() {
     return !deepEqual(this.options, this.lastOptions);
   }
 
-  validateArguments() {
+  validateArguments(positional, named) {
     assert(
       `'{{in-viewport}}' does not accept positional parameters. Specify listeners via 'onEnter' / 'onExit'.`,
-      this.args.positional.length === 0
+      positional.length === 0
     );
     assert(
       `'{{in-viewport}}' either expects 'onEnter', 'onExit' or both to be present.`,
-      typeof this.args.named.onEnter === 'function' ||
-        typeof this.args.named.onExit === 'function'
+      typeof named.onEnter === 'function' || typeof named.onExit === 'function'
     );
   }
 
   @action
   onEnter(...args) {
-    if (this.args.named.onEnter) {
-      this.args.named.onEnter.call(null, this.element, ...args);
+    if (this.onEnterArg) {
+      this.onEnterArg.call(null, this.savedElement, ...args);
     }
 
     if (!this.options.viewportSpy) {
-      this.inViewport.stopWatching(this.element);
+      this.inViewport.stopWatching(this.savedElement);
     }
   }
 
   @action
   onExit(...args) {
-    if (this.args.named.onExit) {
-      this.args.named.onExit.call(null, this.element, ...args);
+    if (this.onExitArg) {
+      this.onExitArg.call(null, this.savedElement, ...args);
     }
   }
 
   setupWatcher() {
     assert(
-      `'${this.element}' is already being watched. Make sure that '{{in-viewport}}' is only used once on this element and that you are not calling 'inViewport.watchElement(element)' in other places.`,
-      !WATCHED_ELEMENTS.has(this.element)
+      `'${this.savedElement}' is already being watched. Make sure that '{{in-viewport}}' is only used once on this savedElementand that you are not calling 'inViewport.watchElement(element)' in other places.`,
+      !WATCHED_ELEMENTS.has(this.savedElement)
     );
-    if (DEBUG) WATCHED_ELEMENTS.add(this.element);
+    if (DEBUG) WATCHED_ELEMENTS.add(this.savedElement);
     this.inViewport.watchElement(
-      this.element,
+      this.savedElement,
       this.options,
       this.onEnter,
       this.onExit
@@ -69,27 +84,24 @@ export default class InViewportModifier extends Modifier {
     this.lastOptions = this.options;
   }
 
+  @action
   destroyWatcher() {
-    if (DEBUG) WATCHED_ELEMENTS.delete(this.element);
-    this.inViewport.stopWatching(this.element);
+    if (DEBUG) WATCHED_ELEMENTS.delete(this.savedElement);
+    this.inViewport.stopWatching(this.savedElement);
   }
 
-  didInstall() {
-    this.setupWatcher();
-  }
+  modify(element, positionalArgs, namedArgs) {
+    this.validateArguments(positionalArgs, namedArgs);
+    this.setOptions(namedArgs);
 
-  didUpdateArguments() {
-    if (this.hasStaleOptions) {
+    this.savedElement = element;
+
+    if (!this.isInstalled) {
+      this.setupWatcher();
+      this.isInstalled = true;
+    } else if (this.hasStaleOptions) {
       this.destroyWatcher();
       this.setupWatcher();
     }
-  }
-
-  didReceiveArguments() {
-    this.validateArguments();
-  }
-
-  willRemove() {
-    this.destroyWatcher();
   }
 }
